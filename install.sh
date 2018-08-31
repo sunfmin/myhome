0755c131b5b6d1d46977b2660c341ee3c616f5e3
Felix Sun <sunfmin@gmail.com>
2018-08-31 15:36:51 +0800

    Add entr to replace modd

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-brew install fzf fd rg bat
+brew install fzf fd rg bat entr
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.felix.bash > ~/.felix.bash
