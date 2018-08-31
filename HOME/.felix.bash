64b98e43b151fad329659c94d46f1fc8432192d0
Felix Sun <sunfmin@gmail.com>
2018-08-31 15:12:00 +0800

    Add install.sh

---
+++ HOME/.felix.bash
@@ -0,0 +1,13 @@
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$PATH
+export CDPATH=$GOPATH/src/github.com/theplant:$GOPATH/src/github.com/sunfmin:$GOPATH/src/github.com:$GOPATH/src/bitbucket.org/sunfmin
+export APPKIT_LOG_HUMAN=true
+
+alias less=bat
+alias find=fd
+alias ping='prettyping --nolegend'
+alias preview="fzf --preview 'bat --color \"always\" {}'"
+# add support for ctrl+o to open selected file in VS Code
+export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
+
+test -f ~/.fzf.bash && source ~/.fzf.bash
---
+++ install.sh
@@ -0,0 +1,3 @@
+brew install fzf fd rg bat
+curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
+curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.felix.bash > ~/.felix.bash
