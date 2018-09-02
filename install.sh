ab69cc8fa600132191000cf82a27223194d1e49d
Felix Sun <sunfmin@gmail.com>
2018-09-02 09:40:12 +0800

    Make shell very long

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+shopt -s histappend # https://www.digitalocean.com/community/tutorials/how-to-use-bash-history-commands-and-expansions-on-a-linux-vps
 brew install fzf fd rg bat entr
 yes | /usr/local/opt/fzf/install --no-fish --no-zsh --no-update-rc
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
