e346dd0070a107e6cd239e4f9bfb65877f9e997c
Felix Sun <sunfmin@gmail.com>
2018-09-02 09:36:38 +0800

    Make large shell history

--- HOME/.felix.bash
+++ HOME/.felix.bash
@@ -3,6 +3,9 @@ export PATH=$GOPATH/bin:$PATH
 export CDPATH=$GOPATH/src/github.com/theplant:$GOPATH/src/github.com/sunfmin:$GOPATH/src/github.com:$GOPATH/src/bitbucket.org/sunfmin
 export APPKIT_LOG_HUMAN=true
 
+export HISTSIZE=500000
+export HISTFILESIZE=1000000
+
 alias less=bat
 alias find=fd
 alias ping='prettyping --nolegend'
