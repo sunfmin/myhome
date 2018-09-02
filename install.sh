644fc8179e6222930b4419021271c8b3d14fa72f
Felix Sun <sunfmin@gmail.com>
2018-09-02 09:30:07 +0800

    Add install fzf autocompletion

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 brew install fzf fd rg bat entr
+yes | /usr/local/opt/fzf/install --no-fish --no-zsh --no-update-rc
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.felix.bash > ~/.felix.bash
