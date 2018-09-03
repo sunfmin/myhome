8c58fb55adc642ef8acd0785912439031e43b8b7
Felix Sun <sunfmin@gmail.com>
2018-09-03 14:17:59 +0800

    Add editor, docker-rm-exited

--- HOME/.felix.bash
+++ HOME/.felix.bash
@@ -2,16 +2,21 @@ export GOPATH=$HOME/go
 export PATH=$GOPATH/bin:$PATH
 export CDPATH=$GOPATH/src/github.com/theplant:$GOPATH/src/github.com/sunfmin:$GOPATH/src/github.com:$GOPATH/src/bitbucket.org/sunfmin
 export APPKIT_LOG_HUMAN=true
+export EDITOR="code --wait"
 
 export HISTSIZE=500000
 export HISTFILESIZE=1000000
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
+alias docker-rm-exited='docker ps --filter "status=exited" -q | xargs docker rm'
+alias k=kubectl
 alias less=bat
 alias find=fd
 alias ping='prettyping --nolegend'
 alias preview="fzf --preview 'bat --color \"always\" {}'"
+
 # add support for ctrl+o to open selected file in VS Code
 export FZF_DEFAULT_OPTS="--bind='ctrl-o:execute(code {})+abort'"
+
 
 test -f ~/.fzf.bash && source ~/.fzf.bash
