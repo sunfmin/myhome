1e45c34d44159b3ad04af9abd2bcea0e21ae8227
Felix Sun <sunfmin@gmail.com>
2018-09-06 22:09:35 +0800

    Add jq

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 shopt -s histappend # https://www.digitalocean.com/community/tutorials/how-to-use-bash-history-commands-and-expansions-on-a-linux-vps
-brew install fzf fd rg bat entr prettyping
+brew install fzf fd rg bat entr prettyping jq
 yes | /usr/local/opt/fzf/install --no-fish --no-zsh --no-update-rc
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.felix.bash > ~/.felix.bash
