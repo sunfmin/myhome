88ade9e6e2455a6f43e51677ed4d7b05d8ef08b2
Felix Sun <sunfmin@gmail.com>
2018-12-10 16:50:46 +0800

    Change to docker-cleanup

--- HOME/.felix.bash
+++ HOME/.felix.bash
@@ -9,7 +9,7 @@ export HISTFILESIZE=1000000
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 alias docker-rm-exited='docker ps --filter "status=exited" -q | xargs docker rm'
-alias docker-rmi-untagged='docker rmi $(docker images | grep "^<none>" | awk "{print $3}")'
+alias docker-cleanup='docker rmi $(docker ps -a -q) && docker image prune --force'
 alias k=kubectl
 alias kl=kubeless
 alias less=bat
