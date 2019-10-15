48f505a2550967011e4fd63241f184737844b659
Felix Sun <sunfmin@gmail.com>
2019-10-15 17:42:31 +0800

    Update .felix.bash

--- HOME/.felix.bash
+++ HOME/.felix.bash
@@ -6,7 +6,6 @@ export EDITOR="code --wait"
 
 export HISTSIZE=500000
 export HISTFILESIZE=1000000
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 transfer() {
     curl -H "Max-Downloads: 3" --progress-bar --upload-file "$1" https://transfer.sh/$(basename "$1") | tee /dev/null;
