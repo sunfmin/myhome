049034a311e45a96e5dbd5be22f6d68080e32fb8
Felix Sun <sunfmin@gmail.com>
2021-09-20 13:13:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
 shopt -s histappend # https://www.digitalocean.com/community/tutorials/how-to-use-bash-history-commands-and-expansions-on-a-linux-vps
 brew install fzf fd rg bat entr prettyping jq
-yes | /usr/local/opt/fzf/install --no-fish --no-zsh --no-update-rc
+yes | /opt/homebrew/opt/fzf/install --no-fish --no-bash --no-update-rc
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.slate > ~/.slate
 curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/HOME/.felix.bash > ~/.felix.bash
-if ! grep ".felix.bash" ~/.profile; then
-  echo 'test -f ~/.felix.bash && source ~/.felix.bash' | tee -a ~/.profile
+if ! grep ".felix.bash" ~/.zshrc; then
+  echo 'test -f ~/.felix.bash && source ~/.felix.bash' | tee -a ~/.zshrc
   echo "Added to .profile"
 fi
-
-sudo bash -c "curl -fsSL https://raw.githubusercontent.com/sunfmin/myhome/master/etc/hosts > /etc/hosts"
